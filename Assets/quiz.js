7e7a4a08657cea95dc7a32cf89d81f17a3a41421
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-13 15:39:04 -0400

    Added result display and func to check anwer

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -7,28 +7,68 @@ const answ1El = document.querySelector('#answer1');
 const answ2El = document.querySelector('#answer2');
 const answ3El = document.querySelector('#answer3');
 const answ4El = document.querySelector('#answer4');
+const resultEl = document.querySelector('div.result-display');
 // An array of objects containing each questions along with all answer choices and the actual answer.
 const questions = [
     {
         question: "question1?",
-        answer: "answer1",
+        answer: "answer3",
         choices: ["answer1", "answer2", "answer3", "answer4"]
     },
     {
         question: "question2?",
+        answer: "answer1",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question3?",
+        answer: "answer4",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question4?",
+        answer: "answer3",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question5?",
+        answer: "answer2",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question6?",
+        answer: "answer4",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question7?",
+        answer: "answer1",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question8?",
+        answer: "answer1",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question9?",
+        answer: "answer3",
+        choices: ["answer1", "answer2", "answer3", "answer4"]
+    },
+    {
+        question: "question10?",
         answer: "answer2",
         choices: ["answer1", "answer2", "answer3", "answer4"]
     }
 ];
 let index = 0; // The index for the questions array.
 let score = 0;
-
-
+let timeleft = 120;
 
 // This function starts the quiz when the start button is clicked.
 function startQuiz() {
-    // Add time to countdown timer.
-    let timeleft = 120;
+    // Show countdown timer.
+    countdownEl.style.visibility = "visible";
 
     startButtonEl.style.display = "none";
 
@@ -46,7 +86,7 @@ function startQuiz() {
     sendQuestions(index);
 }
 
-// This function will use the questions array to send out questions and answers.
+// This function will use the questions array to send a questions with a set of answer choices.
 function sendQuestions(index) {
     instructionsEl.style.display = "none";  // Removes the title and instructions.
     quizContainer.style.display = "block";  // Shows the question and answers
@@ -58,34 +98,61 @@ function sendQuestions(index) {
     answ2El.textContent = quest.choices[1];
     answ3El.textContent = quest.choices[2];
     answ4El.textContent = quest.choices[3];
-
-    index++;  // Increments the index so that the next questions will come up the next time this function is called.
 }
 
-// Need function to check if the answer is correct... compare to answer in object
-function checkAnswer(index) {
+// Checks if the answer chosen is correct... compare to answer in object
+function checkAnswer(questIndex, answerIndex) {
+    const quest = questions[questIndex]; // Determines which object in the array (which question)
+
+    // If the index of the answer choice is the same as the actual answer...
+    if (quest.choices[answerIndex] === quest.answer) {
+        console.log('Yay, correct'); // TO BE REMOVED
+        score += 10;
+        // Display "Correct!" for two seconds
+        resultEl.style.display = "block";
+        resultEl.children[1].textContent = "Correct!"
+    } else {
+        console.log('WRONG!');  // TO BE REMOVED
+        // Display "Incorrect :(" for two seconds
+        timeleft -= 10;
+        resultEl.style.display = "block";
+        resultEl.children[1].textContent = "Incorrect :(";
+    }
+
+    index++;  // Increments the index so that the next question will come up the next time sendQuestions is called.
+
+    // After 2 seconds, reset the result to blank, remove it from display, and get new question/answers or end game
+    setTimeout(function() {
+        resultEl.children[1].textContent = "";
+        resultEl.style.display = "none";
+        if (index < 10) {
+            sendQuestions(index);
+        } else {
+            endGame();
+        }
+    }, 2000);
+}
+
+// need a function to end the game
+function endGame() {
 
 }
 
-// Event listeners:
+// Event listeners for the start buttons and for the 4 different answer choices.
 startButtonEl.addEventListener("click", function() {
     startQuiz();
 });
-
 answ1El.addEventListener("click", function() {
-    checkAnswer(0);
+    checkAnswer(index, 0);
 });
-
 answ2El.addEventListener("click", function() {
-    checkAnswer(1);
+    checkAnswer(index, 1);
 });
-
 answ3El.addEventListener("click", function() {
-    checkAnswer(2);
+    checkAnswer(index, 2);
 });
-
 answ4El.addEventListener("click", function() {
-    checkAnswer(3);
+    checkAnswer(index, 3);
 });
 
 
